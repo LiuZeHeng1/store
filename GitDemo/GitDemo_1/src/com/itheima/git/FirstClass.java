c4ca8bec6bb318f4fbec02aaa6a79e2507a8b4a4
LiuZeHeng1 <qq2590757239@outlook.com>
2020-06-12 11:24:35 +0800

    把本地工程添加到git

---
+++ GitDemo/GitDemo_1/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,14 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hell git project");
+        System.out.println("hell git project");
+        System.out.println("hell git project");
+        System.out.println("hell git project");
+        System.out.println("hell git project");
+        System.out.println("hell  git project");
+
+
+    }
+}
